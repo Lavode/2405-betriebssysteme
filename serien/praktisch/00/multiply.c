98f224bb1382a0da34a3f9ee3e87a17083fd3cc0
Michael Senn <michael@senn.email>
2017-02-21 10:45:49 +0100

    Initial commit.

---
+++ serien/praktisch/00/multiply.c
@@ -0,0 +1,18 @@
+/* gcc -Wall net.c */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	uint8_t num0;
+	uint8_t num1;
+	uint8_t sum;
+
+	/* TODO: Implementation */
+
+	printf("%u * %u = %u\n", num0, num1, sum);
+
+	return 0;
+}
