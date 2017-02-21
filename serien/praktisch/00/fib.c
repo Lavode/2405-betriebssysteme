63d3136a1bb8613205ce67ff331bc1e65db4c54d
Michael Senn <michael@senn.email>
2017-02-21 18:24:50 +0100

    Add Fibonacci generator.

--- serien/praktisch/00/fib.c
+++ serien/praktisch/00/fib.c
@@ -4,12 +4,16 @@
 #include <stdint.h>
 
 int fib(int a) {
-	/* TODO: implementation */
+	if (a == 1 || a == 2) {
+		return 1;
+	} else {
+		return fib(a - 1) + fib(a - 2);
+	}
+	return a;
 }
 
-int main(void)
-{
-	/* TODO: implementation */
+int main(void) {
+	printf("%i", fib(10));
 
 	return 0;
 }
