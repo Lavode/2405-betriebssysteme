4802471c6e403cb688b1bfe9112ea531278849e2
Michael Senn <michael@senn.email>
2017-02-21 22:06:57 +0100

    Unify cli and mutliply. Fix compiler warnings.

--- serien/praktisch/00/multiply.c
+++ serien/praktisch/00/multiply.c
@@ -4,13 +4,23 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
+	// 3, as first argument is program name
+	if (argc != 3) {
+		printf("Usage: ./cli <arg1> <arg2>");
+		exit(2);
+	}
+
 	uint8_t num0;
 	uint8_t num1;
 	uint8_t sum;
 
-	/* TODO: Implementation */
+	char *bin;
+	// We'll just discard the rest. Could check if it isn't empty and raise
+	// an error if so.
+	num0 = strtol(argv[1], &bin, 10);
+	num1 = strtol(argv[2], &bin, 10);
+	sum = num0 + num1;
 
 	printf("%u * %u = %u\n", num0, num1, sum);
 
