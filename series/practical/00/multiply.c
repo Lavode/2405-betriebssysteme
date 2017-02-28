1116cebaec27c6c0932183ea5d9f89c49923c540
Michael Senn <michael@senn.email>
2017-02-28 10:27:11 +0100

    Multiply: Catch invalid user input.

--- series/practical/00/multiply.c
+++ series/practical/00/multiply.c
@@ -4,6 +4,11 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+void usage() {
+	printf("Usage: ./cli <arg1> <arg2>\n");
+	exit(2);
+}
+
 int main(int argc, char *argv[]) {
 	uint8_t num0;
 	uint8_t num1;
@@ -13,8 +18,7 @@ int main(int argc, char *argv[]) {
 
 	/* 3, as first argument is program name */
 	if (argc != 3) {
-		printf("Usage: ./cli <arg1> <arg2>\n");
-		exit(2);
+		usage();
 	}
 
 	/* We'll just discard the rest. Could check if it isn't empty and raise
@@ -25,9 +29,11 @@ int main(int argc, char *argv[]) {
 	/* Unsigned ints, so no need to check for < 0. Those will underflow and
 	 * - generally - be caught by the > 9 condition.  What we can't catch
 	 * here, however, is the user entering e.g. -250, which will end up
-	 * as 6. */
-	if (num0 > 9 || num1 > 9) {
-		printf("Both arguments must be between 0 and 9\n");
+	 * as 6. 
+	 * In addition we disallow 0 - as that one's the return value of strtol
+	 * in case of parsing failiure. */
+	if (num0 > 9 || num1 > 9 || num0 == 0 || num1 == 0) {
+		printf("Both arguments must be integers between 1 and 9\n");
 		exit(2);
 	}
 	product = num0 * num1;
